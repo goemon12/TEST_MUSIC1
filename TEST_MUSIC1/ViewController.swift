03845bea0597f5ea0e18f5ed1081fbd6df7f529a
Tadahiro Kato <goemon12.tc4@gmail.com>
2020-01-13 19:52:25 +0900

    Initial Commit

---
+++ TEST_MUSIC1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TEST_MUSIC1
+//
+//  Created by goemon12 on 2020/01/13.
+//  Copyright © 2020 goemon12. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
